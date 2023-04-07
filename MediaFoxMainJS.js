966cc73c2db03cecb9f41097270194c1bb78a416
Pahomk <115905749+Pahomk@users.noreply.github.com>
2023-04-07 16:42:42 +0300

    Update MediaFoxMainJS.js

--- MediaFoxMainJS.js
+++ MediaFoxMainJS.js
@@ -1,13 +1,26 @@
-function connectUiAlert()
-{
+function connectLink(link, callback) {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://raw.githubusercontent.com/Pahomk/mediafox/main/uiAlert.js", true);
+    xhr.open("GET", link, true);
     xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            eval(xhr.responseText);
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                var script = document.createElement("script");
+                script.text = xhr.responseText;
+                document.head.appendChild(script);
+                callback(null); // Вызываем callback без ошибки
+            } else {
+                callback(new Error("ERROR: " + xhr.statusText));
+            }
         }
     };
     xhr.send();
 }
-connectUiAlert();
-uiAlert();
+
+//uiAlert
+connectLink("https://raw.githubusercontent.com/Pahomk/mediafox/main/uiAlert.js", function(err) {
+    if (err) {
+        document.body.innerHTML="FATAL SCRIPTS ERROR";
+    } else {
+        uiAlert();
+    }
+});
