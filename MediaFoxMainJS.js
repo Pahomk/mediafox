b996a690ac53c0d09cc50eed93ad2f003b2d80c6
Pahomk <115905749+Pahomk@users.noreply.github.com>
2023-04-07 16:26:17 +0300

    Update MediaFoxMainJS.js

--- MediaFoxMainJS.js
+++ MediaFoxMainJS.js
@@ -1,1 +1,12 @@
-
+function connectUiAlert()
+{
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://raw.githubusercontent.com/Pahomk/mediafox/main/uiAlert.js", true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            eval(xhr.responseText);
+        }
+    };
+    xhr.send();
+}
+connectUiAlert();
