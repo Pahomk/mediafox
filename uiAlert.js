566cf3547cc7e42bcdda5d79bfb4b36927f1d21c
Pahomk <115905749+Pahomk@users.noreply.github.com>
2023-04-07 17:23:27 +0300

    Update uiAlert.js

--- uiAlert.js
+++ uiAlert.js
@@ -1,10 +1,11 @@
 function uiAlert()
 {
-    var btnJoke = ['&#x1F44D','ОК','Понятно','Хорошо','Закрыть'];
-    document.body.innerHTML =  "<div class='ui-shadow'>"+document.body.innerHTML+"</div>"
+        var btnJoke = ['&#x1F44D','ОК','Понятно','Хорошо','Закрыть'];
+    document.body.innerHTML =  "<div class='ui-body-shadow'>"+document.body.innerHTML+"</div>"
     document.body.innerHTML = "<div class='ui-Alert-block' style='position: absolute; left: 0; top: -400px; right: 0; display: flex; justify-content: center; margin-top:10px; transition: all 1s ease-in-out 0.5s'><div class='ui-Alert-content ui-box ui-text-sm ui-font-bold' style='position: sticky; z-index:999; text-align: center;'> Welcome to MediaFox &#x1F98A <br> <div class='ui-text-sm ui-font-medium' stlye='padding:0px; margin:0px'><div style='margin: 2px 0 1px 0'>Проект в рамках курсовой работы</div></div> <div><input class='ui-button ui-button-grand' style='width: 100%; margin-top:10px' type='button' value='"+btnJoke[Math.floor(Math.random() * btnJoke.length-1)+1]+"'/></div></div></div>"+document.body.innerHTML;
-    document.querySelector(".ui-shadow").classList.add('catagory-shadow--active');
-    var AlertBlock = document.body.querySelector('.ui-Alert-block');
+    let bodyShadow=document.querySelector(".ui-body-shadow");
+    let AlertBlock = document.body.querySelector('.ui-Alert-block');
+    bodyShadow.classList.add('ui-shadow--active');
     AlertBlock.style.top="0";
     function freezeScroll(event)
     {
@@ -15,12 +16,12 @@ function uiAlert()
     function closeAlert()
     {
         AlertBlock.style.top="-400px";
-        document.querySelector(".ui-shadow").classList.add('catagory-shadow--remove');
+        bodyShadow.classList.add('ui-shadow--remove');
         setTimeout(()=>{
             document.removeEventListener('scroll',freezeScroll);
             document.body.style.overflow="auto";
-            document.querySelector(".ui-shadow").classList.remove('catagory-shadow--remove');
-            document.querySelector(".ui-shadow").classList.remove('catagory-shadow--active');
+            bodyShadow.classList.remove('ui-shadow--remove');
+            bodyShadow.classList.remove('ui-shadow--active');
             AlertBlock.remove();
         },1000);
     }
