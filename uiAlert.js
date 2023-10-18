a46feb325c08e6f829f9471a252eb17433b0c256
Pahomk <115905749+Pahomk@users.noreply.github.com>
2023-10-18 22:38:13 +0300

    Update uiAlert.js
    Обновление содержания уведомления

--- uiAlert.js
+++ uiAlert.js
@@ -1,6 +1,6 @@
 function uiAlert()
 {
-            var btnJoke = ['&#x1F44D','ОК','Понятно','Хорошо','Закрыть'];
+            var btnJoke = ['&#x1F44D','ОК','Понятно','Хорошо','Закрыть', 'Прекрасно'];
             var body = document.body;
 
             var bodyCreateShadow = document.createElement('div');
@@ -13,7 +13,7 @@ function uiAlert()
             var uiAlertContent = document.createElement('div');
             uiAlertContent.classList.add('ui-Alert-block')
             uiAlertContent.style = "position: absolute; left: 0; top: -400px; right: 0; display: flex; justify-content: center; margin-top:10px; transition: all 1s ease-in-out 0.5s";
-            uiAlertContent.innerHTML = "<div class='ui-Alert-content ui-box ui-text-sm ui-font-bold' style='position: sticky; z-index:999; text-align: center;'> Welcome to MediaFox &#x1F98A <br> <div class='ui-text-sm ui-font-medium' stlye='padding:0px; margin:0px'><div style='margin: 2px 0 1px 0'>Проект в рамках курсовой работы</div></div> <div><input class='ui-button ui-button-grand' style='width: 100%; margin-top:10px' type='button' value='"+btnJoke[Math.floor(Math.random() * btnJoke.length-1)+1]+"'/></div></div>";
+            uiAlertContent.innerHTML = "<div class='ui-Alert-content ui-box ui-text-sm ui-font-bold' style='position: sticky; z-index:999; text-align: center;'> Welcome to MediaFox &#x1F98A <br> <div class='ui-text-sm ui-font-medium' stlye='padding:0px; margin:0px'><div style='margin: 2px 0 1px 0'>Iphone 15 УЖЕ В ПРОДАЖЕ! с 23 сентября</div> <div style='margin: 2px 0 1px 0'><img src='https://i.ibb.co/hLmtzPg/image-Photo-Room-png-Photo-Room.png' alt='Изображение Iphone 15'/></div></div> <div><input class='ui-button ui-button-grand' style='width: 100%; margin-top:10px' type='button' value='"+btnJoke[Math.floor(Math.random() * btnJoke.length-1)+1]+"'/></div></div>";
             document.body.prepend(uiAlertContent);
 
             let bodyShadow=document.querySelector(".ui-body-shadow");
